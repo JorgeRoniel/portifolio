7ac04b3b9805ad8eb129dcc318314fb39ff8f8fe
JorgeRoniel <jorge.infor4g@gmail.com>
2025-05-12 21:29:59 -0300

    feat: Projeto quase finalizado...

---
+++ src/js/scripts.js
@@ -0,0 +1,35 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal("#home", {
+  duration: 2000,
+});
+
+sr.reveal("#img-perfil", {
+  origin: "right",
+  distance: "100px",
+  duration: 2000,
+});
+
+sr.reveal("#content-home", {
+  duration: 2000,
+});
+
+sr.reveal("#curriculo-info", {
+  origin: "left",
+  distance: "100px",
+  duration: 1000,
+});
+
+sr.reveal("#curriculo-habilidades", {
+  origin: "right",
+  distance: "100px",
+  duration: 1000,
+});
+
+sr.reveal("#curriculo", {
+  duration: 2000,
+});
+
+sr.reveal("#projetos", {
+  duration: 2000,
+});
